ca317cdad5be7ff99c7e4183dd1c36331c0a610c
Soumya Banerjee <neel.soumya@gmail.com>
2021-01-13 08:54:36 +0000

    Update RUN.R

--- RUN.R
+++ RUN.R
@@ -1,3 +1,8 @@
+##########################################
+# Script to start shiny server
+#   assumes library already installed
+##########################################
+
 library(dsSurvivalShiny)
 
 dsSurvivalShiny::app()
