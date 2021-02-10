38e534e1ddd9735d949f97e16a5ce47cabc0e75b
Soumya Banerjee <neel.soumya@gmail.com>
2021-02-10 10:14:21 +0000

    Update server.R

--- inst/shinyApp/server.R
+++ inst/shinyApp/server.R
@@ -208,7 +208,8 @@ shinyServer(function(input, output) {
                     coxph_model_full$study9$coefficients[1,2],
                     coxph_model_full$study10$coefficients[1,2],
                     coxph_model_full$study11$coefficients[1,2],
-                    coxph_model_full$study12$coefficients[1,2]
+                    coxph_model_full$study12$coefficients[1,2],
+                    coxph_model_full$study13$coefficients[1,2]
     )
     
     # list of standard errors for third parameter over all 7 studies 
@@ -222,7 +223,8 @@ shinyServer(function(input, output) {
                     coxph_model_full$study9$coefficients[1,3],
                     coxph_model_full$study10$coefficients[1,3],
                     coxph_model_full$study11$coefficients[1,3],
-                    coxph_model_full$study12$coefficients[1,3]
+                    coxph_model_full$study12$coefficients[1,3],
+                    coxph_model_full$study13$coefficients[1,3]
     )
     
     
@@ -238,7 +240,7 @@ shinyServer(function(input, output) {
     metafor::forest.rma(x = meta_model,
                         digits = 6, # 6 decimal places round
                         at = c(0.996, 1, 1.004, 1.008),   # ticks for hazard ratio at these places
-                        slab = c('France', 'Italy', 'Spain', 'UK', 'Netherlands', 'Germany', 'Sweden', 'Denmark', 'WHI', 'CARDIA', 'Golestan'))  # Denmark, WHI
+                        slab = c('France', 'Italy', 'Spain', 'UK', 'Netherlands', 'Germany', 'Sweden', 'Denmark', 'WHI', 'CARDIA', 'Golestan', 'MESA'))  # Denmark, WHI
     
     ############################################# 
     # TODO: save plot
