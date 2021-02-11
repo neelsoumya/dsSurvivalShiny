6352380736c6e21e0d8bdf57d3bd4b7218468d79
Soumya Banerjee <neel.soumya@gmail.com>
2021-02-11 09:53:42 +0000

    Update redmeat_survival_ownVM.R

--- inst/shinyApp/redmeat_survival_ownVM.R
+++ inst/shinyApp/redmeat_survival_ownVM.R
@@ -103,8 +103,14 @@ builder$append(server = "study13",
                user = "soumya", password = "interconnect2021",
                table = "MEAT.mesa_pattern_harm", driver = "OpalDriver")
 
+# PRHHP data
+builder$append(server = "study14",
+               url = "http://opal-dev.mrc-epid.cam.ac.uk:8080",
+               user = "soumya", password = "interconnect2021",
+               table = "MEAT.prhhp_pattern_harm", driver = "OpalDriver")
+
 # MEC data
-# builder$append(server = "study14",
+# builder$append(server = "study15",
 #                url = "http://opal-dev.mrc-epid.cam.ac.uk:8080",
 #                user = "soumya", password = "interconnect2021",
 #                table = "MEAT.mec_pattern_harm", driver = "OpalDriver")
