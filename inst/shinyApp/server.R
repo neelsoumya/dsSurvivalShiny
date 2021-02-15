478ded285e44e9c3928886be85c883a1df649ddd
Soumya Banerjee <neel.soumya@gmail.com>
2021-02-15 11:01:08 +0000

    Update server.R

--- inst/shinyApp/server.R
+++ inst/shinyApp/server.R
@@ -210,8 +210,8 @@ shinyServer(function(input, output) {
                     coxph_model_full$study11$coefficients[1,2],
                     coxph_model_full$study12$coefficients[1,2],
                     coxph_model_full$study13$coefficients[1,2],
-                    coxph_model_full$study14$coefficients[1,2],
-                    coxph_model_full$study16$coefficients[1,2]
+                    coxph_model_full$study14$coefficients[1,2]#,
+                    # coxph_model_full$study16$coefficients[1,2]
     )
     
     # list of standard errors for third parameter over all 7 studies 
@@ -227,8 +227,8 @@ shinyServer(function(input, output) {
                     coxph_model_full$study11$coefficients[1,3],
                     coxph_model_full$study12$coefficients[1,3],
                     coxph_model_full$study13$coefficients[1,3],
-                    coxph_model_full$study14$coefficients[1,3],
-                    coxph_model_full$study16$coefficients[1,3]
+                    coxph_model_full$study14$coefficients[1,3]#,
+                    # coxph_model_full$study16$coefficients[1,3]
     )
     
     
@@ -245,7 +245,7 @@ shinyServer(function(input, output) {
                         digits = 6, # 6 decimal places round
                         at = c(0.992, 0.996, 1, 1.004, 1.008),   # ticks for hazard ratio at these places
                         # at = c(0.996, 1, 1.004, 1.008),   # ticks for hazard ratio at these places
-                        slab = c('France', 'Italy', 'Spain', 'UK', 'Netherlands', 'Germany', 'Sweden', 'Denmark', 'WHI', 'CARDIA', 'Golestan', 'MESA', 'PRHHP', 'ARIC'))  # , 'PRHHP' Denmark, WHI
+                        slab = c('France', 'Italy', 'Spain', 'UK', 'Netherlands', 'Germany', 'Sweden', 'Denmark', 'WHI', 'CARDIA', 'Golestan', 'MESA', 'PRHHP')) #, 'ARIC'))  # , 'PRHHP' Denmark, WHI
     
     ############################################# 
     # TODO: save plot
