e334019459365ea774c88672dafb956f38e99968
Soumya Banerjee <neel.soumya@gmail.com>
2021-02-17 10:55:30 +0000

    Create build_man_test.R

---
+++ build_man_test.R
@@ -0,0 +1,25 @@
+############################################
+# Script to build manuals and test
+#
+# Usage:
+#   R --no-save < build_man_test.R
+#
+############################################
+
+##################
+# load libraries
+##################
+library(devtools)
+library(testthat)
+
+##################
+# build manuals
+##################
+devtools::build_manual()
+
+##################
+# Testing
+##################
+devtools::test()
+
+
