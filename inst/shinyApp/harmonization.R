1d2dec2a6aa1eb961cfb70449edd857321367c64
Soumya Banerjee <neel.soumya@gmail.com>
2021-02-22 09:21:45 +0000

    Update harmonization.R

--- inst/shinyApp/harmonization.R
+++ inst/shinyApp/harmonization.R
@@ -6,8 +6,9 @@
 ##################
 # Load library
 ##################
-library(dsBase)
-library(dsBaseClient)
+require('DSI')
+require('DSOpal')
+require('dsBaseClient')
 
 
 #####################################
