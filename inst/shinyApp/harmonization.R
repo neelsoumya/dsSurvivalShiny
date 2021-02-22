8ce0cd1d04b31566e4c68e73c2c542cf1a3724c2
Soumya Banerjee <neel.soumya@gmail.com>
2021-02-22 09:20:29 +0000

    Create harmonization.R

---
+++ inst/shinyApp/harmonization.R
@@ -0,0 +1,18 @@
+############################################
+# Script to perform data harmonization
+#
+############################################
+
+##################
+# Load library
+##################
+library(dsBase)
+library(dsBaseClient)
+
+
+#####################################
+# Harmonize PA (physical activity)
+#####################################
+
+
+ds.ls()
