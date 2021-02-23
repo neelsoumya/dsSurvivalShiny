d5dbc6fda620652f3b327bd8f6dbe533d1c43ab3
Soumya Banerjee <neel.soumya@gmail.com>
2021-02-23 09:49:18 +0000

    coxph models wip

--- inst/shinyApp/harmonization.R
+++ inst/shinyApp/harmonization.R
@@ -16,6 +16,10 @@
 #####################################
 ds.quantileMean(x = 'D_curated$PA', datasources = connections[9]) # WHI
 
-# ds.asFactor(
+ds.asFactor(input.var.name = 'D_curated$PA', newobj.name = 'D_curated$PA', forced.factor.levels = c(0, 3.5, 10, 41.5), datasources = connections[9])
+
+ds.class(x = 'D_curated$PA', datasources = connections[9])
+
+ds.summary(x = 'D_curated$PA', datasources = connections[9])
 
 ds.ls()
