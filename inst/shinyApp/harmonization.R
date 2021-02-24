28ff9566842baa9e34f45672c713eb2a51da6852
Soumya Banerjee <neel.soumya@gmail.com>
2021-02-24 10:47:11 +0000

    Update harmonization.R

--- inst/shinyApp/harmonization.R
+++ inst/shinyApp/harmonization.R
@@ -14,10 +14,17 @@
 #####################################
 # Harmonize PA (physical activity)
 #####################################
+
+# get quantiles for PA for WHI
 ds.quantileMean(x = 'D_curated$PA', datasources = connections[9]) # WHI
 
-ds.asFactor(input.var.name = 'D_curated$PA', newobj.name = 'D_curated$PA', forced.factor.levels = c(0, 3.5, 10, 41.5), datasources = connections[9])
+# assign these quantiles to a temp variable and make it a factor
+ds.asFactor(input.var.name = 'D_curated$PA', newobj.name = 'PA_harmonized', forced.factor.levels = c(0, 3.5, 10, 41.5), datasources = connections[9])
 
+# assign this temp variable (which is now a factor) to original data frame for WHI study
+ds.assign(toAssign = 'PA_harmonized', newobj = 'D_curated$PA', datasources = connections[9])
+
+# check if it is a factor
 ds.class(x = 'D_curated$PA', datasources = connections[9])
 
 ds.summary(x = 'D_curated$PA', datasources = connections[9])
