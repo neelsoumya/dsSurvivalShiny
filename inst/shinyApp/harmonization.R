15969dae83b02edc36877ba6e3681bcdfe5cf830
Soumya Banerjee <neel.soumya@gmail.com>
2021-03-01 11:27:17 +0000

    Update harmonization.R

--- inst/shinyApp/harmonization.R
+++ inst/shinyApp/harmonization.R
@@ -22,11 +22,11 @@ ds.quantileMean(x = 'D_curated$PA', datasources = connections[9]) # WHI
 ds.asFactor(input.var.name = 'D_curated$PA', newobj.name = 'PA_harmonized', forced.factor.levels = c(0, 3.5, 10, 41.5), datasources = connections[9])
 
 # assign this temp variable (which is now a factor) to original data frame for WHI study
-ds.assign(toAssign = 'PA_harmonized', newobj = 'D_curated$PA', datasources = connections[9])
+# ds.assign(toAssign = 'PA_harmonized', newobj = 'D_curated$PA', datasources = connections[9])
 
-ds.dataFrame(x = 'PA_harmonized', newobj = 'D_PA2', datasources = connections[9])
+# ds.dataFrame(x = 'PA_harmonized', newobj = 'D_PA2', datasources = connections[9])
 
-ds.dataFrame(x = 'PA_harmonized', newobj = 'D_curated$PA2', datasources = connections[9])
+# ds.dataFrame(x = 'PA_harmonized', newobj = 'D_curated$PA2', datasources = connections[9])
 
 ds.dataFrame(x = c("D_curated", "PA_harmonized"), newobj = 'D_curated', datasources = connections[9])
 
@@ -34,12 +34,13 @@ ds.dataFrame(x = c("D_curated", "PA_harmonized"), newobj = 'D_curated', datasour
 # https://rdrr.io/github/datashield/dsBaseClient/man/ds.cbind.html
 
 # check if it is a factor
-ds.class(x = 'D_curated$PA', datasources = connections[9])
-ds.class(x = 'PA_harmonized', datasources = connections[9])
-ds.summary(x = 'D_curated$PA', datasources = connections[9])
+# ds.class(x = 'D_curated$PA', datasources = connections[9])
+# ds.class(x = 'PA_harmonized', datasources = connections[9])
+# ds.summary(x = 'D_curated$PA', datasources = connections[9])
 
-ds.class(x = 'D_PA2', datasources = connections[9])
-ds.summary(x = 'D_PA2', datasources = connections[9])
+# ds.class(x = 'D_PA2', datasources = connections[9])
+# ds.summary(x = 'D_PA2', datasources = connections[9])
+ds.summary(x = 'D_curated$PA_harmonized', datasources = connections[9])
 
 # ds.class(x = 'D_curated$PA2', datasources = connections[9])
 # ds.summary(x = 'D_curated$PA2', datasources = connections[9])
