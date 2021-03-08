e4480dec171c98c49ba2056a210d889be32f2cff
Soumya Banerjee <neel.soumya@gmail.com>
2021-03-08 14:58:12 +0000

    Update server.R

--- inst/shinyApp/server.R
+++ inst/shinyApp/server.R
@@ -211,7 +211,7 @@ shinyServer(function(input, output) {
                     coxph_model_full$study12$coefficients[1,2],
                     coxph_model_full$study13$coefficients[1,2],
                     coxph_model_full$study14$coefficients[1,2],
-                    coxph_model_full$study15$coefficients[1,2],
+                    # coxph_model_full$study15$coefficients[1,2],
                     coxph_model_full$study16$coefficients[1,2]
     )
     
@@ -229,7 +229,7 @@ shinyServer(function(input, output) {
                     coxph_model_full$study12$coefficients[1,3],
                     coxph_model_full$study13$coefficients[1,3],
                     coxph_model_full$study14$coefficients[1,3],
-                    coxph_model_full$study15$coefficients[1,3],
+                    # coxph_model_full$study15$coefficients[1,3],
                     coxph_model_full$study16$coefficients[1,3]
     )
     
@@ -247,7 +247,8 @@ shinyServer(function(input, output) {
                         digits = 6, # 6 decimal places round
                         at = c(0.992, 0.996, 1, 1.004, 1.008),   # ticks for hazard ratio at these places
                         # at = c(0.996, 1, 1.004, 1.008),   # ticks for hazard ratio at these places
-                        slab = c('France', 'Italy', 'Spain', 'UK', 'Netherlands', 'Germany', 'Sweden', 'Denmark', 'WHI', 'CARDIA', 'Golestan', 'MESA', 'PRHHP', 'MEC', 'ARIC')) #, 'ARIC'))  # , 'PRHHP' Denmark, WHI
+                        slab = c('France', 'Italy', 'Spain', 'UK', 'Netherlands', 'Germany', 'Sweden', 'Denmark', 'WHI', 'CARDIA', 'Golestan', 'MESA', 'PRHHP', 'ARIC')) #, 'ARIC'))  # , 'PRHHP' Denmark, WHI
+                        # slab = c('France', 'Italy', 'Spain', 'UK', 'Netherlands', 'Germany', 'Sweden', 'Denmark', 'WHI', 'CARDIA', 'Golestan', 'MESA', 'PRHHP', 'MEC', 'ARIC')) #, 'ARIC'))  # , 'PRHHP' Denmark, WHI
     
     ############################################# 
     # TODO: save plot
